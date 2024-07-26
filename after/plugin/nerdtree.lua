4bcf1e9f5ce59c6b07871c78dc38eb0db48a3b1a
Aaron Levy <aaron.levy@aprioriinvestments.com>
2024-07-26 10:03:37 -0400

    Add NERDTreeIgnore

--- after/plugin/nerdtree.lua
+++ after/plugin/nerdtree.lua
@@ -1,3 +1,4 @@
 vim.g.NERDTreeMinimalUI = 1
 vim.keymap.set('n', '<C-n>', ':NERDTreeToggle<CR>', {noremap = true, silent = true})
 vim.keymap.set('n', '<leader>n', ':NERDTreeFind<CR>', {noremap = true, silent = true})
+vim.g.NERDTreeIgnore = {'.*prof.*', '\\.prof', '__pycache__', '\\.pyc$', '\\.o$', '\\.swp',  '.*\\.swp',  'node_modules/', 'tags', '.*\\.asv*'}
