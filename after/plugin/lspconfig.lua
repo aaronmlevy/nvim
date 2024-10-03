b257d3ce7891706ddb85cfda4f19011f99c6ff89
Aaron Levy <aaron.levy@aprioriinvestments.com>
2024-10-03 12:48:19 -0400

    Add matlab lsp

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -11,5 +11,13 @@ lspconfig.pyright.setup{
 }
 
 
-lspconfig.matlab_ls.setup({...})
+lspconfig.matlab_ls.setup{
+    on_attach = function(client, bufnr)
+        local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+        local opts = { noremap=true, silent=true }
+
+        -- Key mappings
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    end
+}
 lspconfig.clangd.setup({...})
