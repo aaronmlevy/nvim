6c2f2bb572037685918c46d5e98dfa6730e89fb4
Aaron Levy <aaron.levy@aprioriinvestments.com>
2025-01-28 14:04:01 -0500

    fixup to pyright

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -8,6 +8,7 @@ lsp.on_attach(
     function(client, bufnr)
         local opts = {buffer=bufnr, remap=false}
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        client.handlers["textDocument/publishDiagnostics"] = function() end  -- Disable diagnostics
     end
 )
 
