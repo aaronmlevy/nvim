44a3ee12a37b4e859b5b6ca62349286c7d4fbed9
Aaron Levy <aaron.levy@aprioriinvestments.com>
2025-07-01 12:09:40 -0400

    fix avante

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -185,9 +185,11 @@ return {
   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   opts = {
      provider = "openai",
-    openai = {
+     providers = {
+       openai = {
          model = "gpt-4o-mini",
        },
+     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
